30a233b9e929e2923a8f23c601ae8f355be310c8
Jacob Leach <leach.jacob@gmail.com>
2018-01-04 19:09:17 -0500

    Fix install script to actually work

--- .vim/install.sh
+++ .vim/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
